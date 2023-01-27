ad7d6da67e65b8ea5df98ddec7cd616f7816419c
TuinderJ <joshua.tuinder@gmail.com>
2023-01-26 19:07:49 -0700

    fixed seeding error

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -2,68 +2,71 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const recipeSchema = new Schema({
-  recipeId: Schema.Types.ObjectId,
-  category: String,
-  cookTime: String,
-  created: String,
-  createdBy: String,
-  customerUuid: String,
-  description: String,
-  favorite: Boolean,
-  images: [String],
-  ingredients: [String],
-  instructions: [
-    {
-      steps: [String],
+const recipeSchema = new Schema(
+  {
+    recipeId: Schema.Types.ObjectId,
+    category: String,
+    cookTime: String,
+    created: String,
+    createdBy: String,
+    customerUuid: String,
+    description: String,
+    favorite: Boolean,
+    images: [String],
+    ingredients: [String],
+    instructions: [
+      {
+        steps: [String],
+      },
+    ],
+    lastUsed: Date,
+    name: {
+      $type: String,
+      required: true,
+      trim: true,
     },
-  ],
-  lastUsed: Date,
-  name: {
-    type: String,
-    required: true,
-    trim: true,
+    newImages: [
+      {
+        hUnits: String,
+        height: Number,
+        length: Number,
+        mime: String,
+        type: String,
+        url: String,
+        wUnits: String,
+        width: Number,
+      },
+    ],
+    newOriginalImages: [
+      {
+        hUnits: String,
+        height: Number,
+        length: Number,
+        mime: String,
+        type: String,
+        url: String,
+        wUnits: String,
+        width: Number,
+      },
+    ],
+    originalDescription: String,
+    originalIngredients: [String],
+    originalInstructions: [
+      {
+        steps: [String],
+      },
+    ],
+    originalName: String,
+    originalTotalTime: String,
+    originalYield: String,
+    prepTime: String,
+    totalTime: String,
+    updated: String,
+    url: String,
+    uuid: String,
+    yield: String,
   },
-  newImages: [
-    {
-      hUnits: String,
-      height: Number,
-      length: Number,
-      mime: String,
-      type: String,
-      url: String,
-      wUnits: String,
-      width: Number,
-    },
-  ],
-  newOriginalImages: [
-    {
-      hUnits: String,
-      height: Number,
-      length: Number,
-      mime: String,
-      type: String,
-      url: String,
-      wUnits: String,
-      width: Number,
-    },
-  ],
-  originalDescription: String,
-  originalIngredients: [String],
-  originalInstructions: [
-    {
-      steps: [String],
-    },
-  ],
-  originalName: String,
-  originalTotalTime: String,
-  originalYield: String,
-  prepTime: String,
-  totalTime: String,
-  updated: String,
-  url: String,
-  uuid: String,
-  yield: String,
-});
+  { typeKey: '$type' }
+);
 
 module.exports = recipeSchema;
